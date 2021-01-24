d0b97955ecb44003ca88c502a37c9e230c014d8a
Pedro Jordão <pedrohjordao@gmail.com>
2021-01-24 09:54:44 +0000

    Removes main and adds basic setup for development

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(generic_associated_types)]
+
+pub trait Functor {
+    type Inner;
+    type Outter<B>: Functor;
+
+    fn map<F, B>(self, f: F) -> Self::Outter<B>
+    where
+        F: FnMut(Self::Inner) -> B;
 }
