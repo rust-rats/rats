b5482fa737905e91d433b02b1b39b0df738db801
Pedro Jordão <pedrohjordao@gmail.com>
2021-01-24 11:05:27 +0000

    Adds allow clause and basic impl of functor for testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![allow(incomplete_features)]
 #![feature(generic_associated_types)]
 
 pub trait Functor {
@@ -7,4 +8,41 @@ pub trait Functor {
     fn map<F, B>(self, f: F) -> Self::Outter<B>
     where
         F: FnMut(Self::Inner) -> B;
+}
+
+impl<A> Functor for Option<A> {
+    type Inner = A;
+    type Outter<B> = Option<B>;
+
+    fn map<F, B>(self, mut f: F) -> Self::Outter<B>
+    where
+        F: FnMut(Self::Inner) -> B,
+    {
+        match self {
+            Some(v) => Some(f(v)),
+            None => None,
+        }
+    }
+}
+
+pub fn lift<A: Functor, B, F>(fun: F) -> impl FnMut(A) -> <A as Functor>::Outter<B>
+where
+    F: FnMut(<A as Functor>::Inner) -> B + Copy,
+{
+    move |a: A| a.map(fun)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn do_you_even_lift() {
+        let times_two = |x: i32| x * 2;
+
+        let mut lifted = lift::<Option<i32>, _, _>(times_two);
+
+        let value = Some(1i32);
+        assert_eq!(lifted(value), Some(2))
+    }
 }
