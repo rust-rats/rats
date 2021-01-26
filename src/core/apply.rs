45072eff54ec01456811a20fad67a1a11dddee21
Luiz Jardim <CTW00767@criticaltechworks.com>
2021-01-26 10:46:47 +0000

    tests for impl Apply for Option

--- src/core/apply.rs
+++ src/core/apply.rs
@@ -33,3 +33,50 @@ impl<A> Apply for Id<A> {
         self.fmap(f.into_value())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::Apply;
+
+    fn to_none<T>(_ : Option<T>) -> Option<T> {
+        None
+    }
+
+    #[test]
+    fn option_some_some() {
+        let option = Some(3);
+        let function = Some(|x : u64| -> f32 {
+            (x*x) as f32
+        });
+        assert_eq!(option.apply(function), Some(9.0));
+    }
+
+    #[test]
+    fn option_some_none() {
+        let option = Some(3);
+        let function = Some(|x : u64| -> f32 {
+            (x*x) as f32
+        });
+        let function = to_none(function);
+        assert_eq!(option.apply(function), None);
+    }
+
+    #[test]
+    fn option_none_some() {
+        let option = None;
+        let function = Some(|x : u64| -> f32 {
+            (x*x) as f32
+        });
+        assert_eq!(option.apply(function), None);
+    }
+
+    #[test]
+    fn option_none_none() {
+        let option = None;
+        let function = Some(|x : u64| -> f32 {
+            (x*x) as f32
+        });
+        let function = to_none(function);
+        assert_eq!(option.apply(function), None);
+    }
+}
