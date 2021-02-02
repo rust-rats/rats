6b613d2b8c25766f70f759e27f15e55b7eb28485
Luiz Jardim <CTW00767@criticaltechworks.com>
2021-02-02 10:17:09 +0000

    formatting

--- src/core/apply.rs
+++ src/core/apply.rs
@@ -1,7 +1,7 @@
 use super::functor::Functor;
 use crate::kernel::prelude::Id;
 
-pub trait Apply : Functor {
+pub trait Apply: Functor {
     fn apply<B, F>(self, f: Self::Outter<F>) -> Self::Outter<B>
     where
         F: FnMut(Self::Inner) -> B;
@@ -10,7 +10,7 @@ pub trait Apply : Functor {
 impl<A> Apply for Option<A> {
     fn apply<B, F>(self, f: Self::Outter<F>) -> Self::Outter<B>
     where
-        F: FnMut(Self::Inner) -> B
+        F: FnMut(Self::Inner) -> B,
     {
         self.fmap(f?)
     }
@@ -18,32 +18,34 @@ impl<A> Apply for Option<A> {
 
 impl<A, E> Apply for Result<A, E> {
     fn apply<B, F>(self, f: Self::Outter<F>) -> Self::Outter<B>
-        where
-            F: FnMut(Self::Inner) -> B
+    where
+        F: FnMut(Self::Inner) -> B,
     {
         self.fmap(f?)
     }
 }
 
-impl<A : Clone> Apply for Vec<A> {
+impl<A: Clone> Apply for Vec<A> {
     fn apply<B, F>(self, mut f: Self::Outter<F>) -> Self::Outter<B>
-        where
-            F: FnMut(Self::Inner) -> B
+    where
+        F: FnMut(Self::Inner) -> B,
     {
-        self.into_iter().flat_map(|value| -> Self::Outter<B> {
-            let mut applied_values = Vec::with_capacity(f.len());
-            for function in f.iter_mut() {
-                applied_values.push(function(value.clone()));
-            };
-            applied_values
-        }).collect()
+        self.into_iter()
+            .flat_map(|value| -> Self::Outter<B> {
+                let mut applied_values = Vec::with_capacity(f.len());
+                for function in f.iter_mut() {
+                    applied_values.push(function(value.clone()));
+                }
+                applied_values
+            })
+            .collect()
     }
 }
 
 impl<A> Apply for Id<A> {
     fn apply<B, F>(self, f: Self::Outter<F>) -> Self::Outter<B>
-        where
-            F: FnMut(Self::Inner) -> B
+    where
+        F: FnMut(Self::Inner) -> B,
     {
         self.fmap(f.into_value())
     }
@@ -53,25 +55,21 @@ impl<A> Apply for Id<A> {
 mod tests {
     use super::Apply;
 
-    fn to_none<T>(_ : Option<T>) -> Option<T> {
+    fn to_none<T>(_: Option<T>) -> Option<T> {
         None
     }
 
     #[test]
     fn option_some_some() {
         let option = Some(3);
-        let function = Some(|x : u64| -> f32 {
-            (x*x) as f32
-        });
+        let function = Some(|x: u64| -> f32 { (x * x) as f32 });
         assert_eq!(option.apply(function), Some(9.0));
     }
 
     #[test]
     fn option_some_none() {
         let option = Some(3);
-        let function = Some(|x : u64| -> f32 {
-            (x*x) as f32
-        });
+        let function = Some(|x: u64| -> f32 { (x * x) as f32 });
         let function = to_none(function);
         assert_eq!(option.apply(function), None);
     }
@@ -79,41 +77,33 @@ mod tests {
     #[test]
     fn option_none_some() {
         let option = None;
-        let function = Some(|x : u64| -> f32 {
-            (x*x) as f32
-        });
+        let function = Some(|x: u64| -> f32 { (x * x) as f32 });
         assert_eq!(option.apply(function), None);
     }
 
     #[test]
     fn option_none_none() {
         let option = None;
-        let function = Some(|x : u64| -> f32 {
-            (x*x) as f32
-        });
+        let function = Some(|x: u64| -> f32 { (x * x) as f32 });
         let function = to_none(function);
         assert_eq!(option.apply(function), None);
     }
 
-    fn to_err<T, E>(_ : Result<T, E>, e: E) -> Result<T, E> {
+    fn to_err<T, E>(_: Result<T, E>, e: E) -> Result<T, E> {
         Err(e)
     }
 
     #[test]
     fn result_ok_ok() {
-        let result : Result<u64, ()> = Ok(3);
-        let function = Ok(|x : u64| -> f32 {
-            (x*x) as f32
-        });
+        let result: Result<u64, ()> = Ok(3);
+        let function = Ok(|x: u64| -> f32 { (x * x) as f32 });
         assert_eq!(result.apply(function), Ok(9.0));
     }
 
     #[test]
     fn result_ok_err() {
-        let result : Result<u64, ()> = Ok(3);
-        let function = Ok(|x : u64| -> f32 {
-            (x*x) as f32
-        });
+        let result: Result<u64, ()> = Ok(3);
+        let function = Ok(|x: u64| -> f32 { (x * x) as f32 });
         let function = to_err(function, ());
         assert_eq!(result.apply(function), Err(()));
     }
@@ -121,18 +111,14 @@ mod tests {
     #[test]
     fn result_err_ok() {
         let result = Err(());
-        let function = Ok(|x : u64| -> f32 {
-            (x*x) as f32
-        });
+        let function = Ok(|x: u64| -> f32 { (x * x) as f32 });
         assert_eq!(result.apply(function), Err(()));
     }
 
     #[test]
     fn result_err_err() {
         let result = Err(());
-        let function = Ok(|x : u64| -> f32 {
-            (x*x) as f32
-        });
+        let function = Ok(|x: u64| -> f32 { (x * x) as f32 });
         let function = to_err(function, ());
         assert_eq!(result.apply(function), Err(()));
     }
@@ -142,21 +128,15 @@ mod tests {
         use crate::kernel::prelude::Id;
 
         let id = Id(3);
-        let function = Id(|x : u64| -> f32 {
-            (x*x) as f32
-        });
+        let function = Id(|x: u64| -> f32 { (x * x) as f32 });
         assert_eq!(id.apply(function), Id(9.0));
     }
 
     #[test]
     fn vec() {
         let values = vec![1, 2, 3, 4];
-        let functions = vec![
-            |x : u64| -> f32 { (x*x) as f32 }
-        ];
-        let expected = vec![
-            1.0, 4.0, 9.0, 16.0,
-        ];
+        let functions = vec![|x: u64| -> f32 { (x * x) as f32 }];
+        let expected = vec![1.0, 4.0, 9.0, 16.0];
         assert_eq!(values.apply(functions), expected);
     }
 }
--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,9 +1,9 @@
+pub mod apply;
 pub mod bifunctor;
 pub mod functor;
-pub mod apply;
 
 pub mod prelude {
+    pub use super::apply::*;
     pub use super::bifunctor::*;
     pub use super::functor::*;
-    pub use super::apply::*;
 }
