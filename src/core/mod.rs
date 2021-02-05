c524ca86fd68126f2bb05eb872b164be4f55fb78
Luiz Jardim <CTW00767@criticaltechworks.com>
2021-02-05 12:37:31 +0000

    applicative implementation and tests

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,9 +1,11 @@
+pub mod applicative;
 pub mod apply;
 pub mod bifunctor;
 pub mod foldable;
 pub mod functor;
 
 pub mod prelude {
+    pub use super::applicative::*;
     pub use super::apply::*;
     pub use super::bifunctor::*;
     pub use super::foldable::*;
