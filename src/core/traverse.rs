64f7de34448cc77fcf61ff9f3af71d30a33c40a8
Pedro Jordão <pedrohjordao@gmail.com>
2021-02-06 11:34:39 +0000

    Compiler goes BOOM

--- src/core/traverse.rs
+++ src/core/traverse.rs
@@ -1,6 +1,6 @@
 use super::prelude::{Applicative, Foldable, Functor};
 
-pub trait Traverse: Functor + Foldable {
+pub trait Traverse: Functor<Outter = Self> + Foldable<Outter = Self> {
     fn traverse<G: Applicative, B, F>(
         self,
         f: F,
@@ -9,21 +9,21 @@ pub trait Traverse: Functor + Foldable {
         F: FnMut(<Self as Functor>::Inner) -> <G as Functor>::Outter<B>;
 }
 
-impl<T> Traverse for Vec<T> {
-    fn traverse<G: Applicative, B, F>(
-        self,
-        f: F,
-    ) -> <G as Functor>::Outter<<Self as Functor>::Outter<B>>
-    where
-        F: FnMut(<Self as Functor>::Inner) -> <G as Functor>::Outter<B>,
-    {
-        if self.is_empty() {
-            G::pure(Vec::<B>::new());
-        } else {
-            self.fold_right(G::pure(Vec::<B>::new()), |acc, b| {
-                //acc.fmap(|mut vec| G::pure(vec.push(b)))
-                todo!()
-            })
-        }
-    }
-}
+// impl<T> Traverse for Vec<T> {
+// fn traverse<G: Applicative, B, F>(
+// self,
+// f: F,
+// ) -> <G as Functor>::Outter<<Self as Functor>::Outter<B>>
+// where
+// F: FnMut(<Self as Functor>::Inner) -> <G as Functor>::Outter<B>,
+// {
+// if self.is_empty() {
+// G::pure(Vec::<B>::new());
+// } else {
+// self.fold_right(G::pure(Vec::<B>::new()), |acc, b| {
+// //acc.fmap(|mut vec| G::pure(vec.push(b)))
+// todo!()
+// })
+// }
+// }
+// }
