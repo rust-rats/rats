6ddaf5f61b1b16a726c349f8d60c8f1033c7b7db
Pedro Jordão <pedrohjordao@gmail.com>
2021-02-14 13:07:49 +0000

    Defines a Kind trait to represent HKTs

--- src/kernel/mod.rs
+++ src/kernel/mod.rs
@@ -25,7 +25,26 @@ pub mod helpers {
     }
 }
 
+pub trait Kind {
+    type Ty<T>;
+}
+
+pub mod default_kinds {
+    use super::Kind;
+
+    pub struct VecKind;
+    impl Kind for VecKind {
+        type Ty<T> = Vec<T>;
+    }
+
+    pub struct OptionKind;
+    impl Kind for OptionKind {
+        type Ty<T> = Option<T>;
+    }
+}
+
 pub mod prelude {
+    pub use super::default_kinds::*;
     pub use super::helpers::*;
     pub use super::monoid::*;
     pub use super::semigroup::*;
